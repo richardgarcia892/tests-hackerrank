860861408399603aec5d41d43852651447c7551a
Richard Garcia <richardgarcia892@gmail.com>
2022-02-16 16:36:23 -0400

    first

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,24 +1,40 @@
+const { isNumber } = require('lodash');
 var recipes = require('../recipes.json');
 var router = require('express').Router();
 
-
-router.get('/', (req, res) => {
-    const {context} = req;
+router.get(
+  '/',
+  (req, res, next) => {
+    const { page, limit, q } = req.query;
+    console.log({ page, limit, q });
+    let context = {};
+    context = {
+      page: isNumber(page) ? parseInt(page) : 1,
+      limit: isNumber(limit) ? parseInt(limit) : 3,
+      skip: (parseInt(page) - 1) * parseInt(limit),
+      searchTerm: q,
+      search: q,
+    };
+    req.context = context;
+    next();
+  },
+  (req, res) => {
+    const { context } = req;
     let results = [];
     if (context.searchTerm) {
-        results = recipes.filter(({name}) => name.match(context.search))
+      results = recipes.filter(({ name }) => name.match(context.search));
     } else {
-        results = recipes
+      results = recipes;
     }
-    results = results.slice(context.skip, (context.skip + context.limit))
+    results = results.slice(context.skip, context.skip + context.limit);
     res.json({
-        page: context.page,
-        limit: context.limit,
-        skip: context.skip,
-        search: context.searchTerm,
-        data: results
+      page: context.page,
+      limit: context.limit,
+      skip: context.skip,
+      search: context.searchTerm,
+      data: results,
     });
-});
+  }
+);
 
 module.exports = router;
-
