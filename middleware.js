927f210bae7ee41d0fb9fb1fea7220a008aa688d
Richard Garcia <richardgarcia892@gmail.com>
2022-02-19 10:25:43 -0400

    Solucionado

--- middleware.js
+++ middleware.js
@@ -1,3 +1,17 @@
-module.exports = (req, res, next) => {
+const { isNumber } = require('lodash');
 
+const PAGE_DEFAULT = 1;
+const LIMIT_DEFAULT = 3;
+
+module.exports = (req, res, next) => {
+  let { page, limit, q } = req.query;
+  page = page ? parseInt(page) : PAGE_DEFAULT;
+  limit = limit ? parseInt(limit) : LIMIT_DEFAULT;
+  const skip = (parseInt(page) - 1) * parseInt(limit);
+  const searchTerm = q;
+  const search = q;
+  console.log({ page, limit, q });
+  const context = { page, limit, skip, searchTerm, search };
+  req.context = context;
+  next();
 };
--- routes/recipes.js
+++ routes/recipes.js
@@ -1,40 +1,23 @@
-const { isNumber } = require('lodash');
+const middleware = require('../middleware');
 var recipes = require('../recipes.json');
 var router = require('express').Router();
 
-router.get(
-  '/',
-  (req, res, next) => {
-    const { page, limit, q } = req.query;
-    console.log({ page, limit, q });
-    let context = {};
-    context = {
-      page: isNumber(page) ? parseInt(page) : 1,
-      limit: isNumber(limit) ? parseInt(limit) : 3,
-      skip: (parseInt(page) - 1) * parseInt(limit),
-      searchTerm: q,
-      search: q,
-    };
-    req.context = context;
-    next();
-  },
-  (req, res) => {
-    const { context } = req;
-    let results = [];
-    if (context.searchTerm) {
-      results = recipes.filter(({ name }) => name.match(context.search));
-    } else {
-      results = recipes;
-    }
-    results = results.slice(context.skip, context.skip + context.limit);
-    res.json({
-      page: context.page,
-      limit: context.limit,
-      skip: context.skip,
-      search: context.searchTerm,
-      data: results,
-    });
+router.get('/', middleware, (req, res) => {
+  const { context } = req;
+  let results = [];
+  if (context.searchTerm) {
+    results = recipes.filter(({ name }) => name.match(context.search));
+  } else {
+    results = recipes;
   }
-);
+  results = results.slice(context.skip, context.skip + context.limit);
+  res.json({
+    page: context.page,
+    limit: context.limit,
+    skip: context.skip,
+    search: context.searchTerm,
+    data: results,
+  });
+});
 
 module.exports = router;
